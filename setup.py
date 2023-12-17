21fe7ab48e9896849dca5373786ee62676307029
Devendra Pratap <dps.manit@gmail.com>
2023-12-17 15:14:35 +0530

    minor format

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ data_files = [os.path.join(data_path, file) for file in os.listdir(data_path) if
 long_description = "Small python utility to generate dummy data in database tables" 
 
 setup( 
-	name ="fake-db-generator", 
-	version ="1.1.0", 
-	author ="Devendra Pratap", 
-	author_email ="dps.manit@gmail.com", 
-	url ="https://github.com/devendrapratap02/fake-db-generator", 
-	description ="Fake DB Generator", 
+	name = "fake-db-generator", 
+	version = "1.1.0", 
+	author = "Devendra Pratap", 
+	author_email = "dps.manit@gmail.com", 
+	url = "https://github.com/devendrapratap02/fake-db-generator", 
+	description = "Fake DB Generator", 
 	long_description = long_description, 
-	long_description_content_type ="text/markdown", 
+	long_description_content_type = "text/markdown", 
 	license = "MIT", 
 	packages = find_packages(), 
 	entry_points = { 
@@ -33,7 +33,7 @@ setup(
 	], 
 	data_files = data_files,
 	include_package_data = True,
-	keywords ="faker dummy db database generate generator dps", 
+	keywords = "faker dummy db database generate generator dps", 
 	install_requires = requirements, 
 	zip_safe = False
 ) 
